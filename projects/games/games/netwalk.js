9c8eeebb22c601b1f9e5e9ffb0d5794b913f423f
Hakerh400 <hakerh403@gmail.com>
2018-04-06 20:12:42 +0200

    commit

--- projects/games/games/netwalk.js
+++ projects/games/games/netwalk.js
@@ -7,6 +7,13 @@ game.draw = (x, y, d, g) => {
   g.fillRect(x, y, 1, 1);
   g.fillStyle = '#00ff00';
   game.tube(x, y, d[0]);
+
+  if(m && d.id === m){
+    g.globalAlpha = .5;
+    g.fillStyle = 'red';
+    g.fillRect(x, y, 1, 1);
+    g.globalAlpha = 1;
+  }
 };
 
 game.export = (x, y, d, bs) => {
@@ -19,18 +26,22 @@ game.import = (x, y, d, bs) => {
   d[1] = bs.read(1);
 };
 
+var m;
+
 game.generate = () => {
   var {w, h} = game;
   game.iterate((x, y, d) => d[0] = d[1] = 0);
   var [x, y] = [w, h].map(a => a >> 1);
-  var id = game.getId();
+  var id = m = game.getId();
   var d = game.get(x, y);
+  d.id1 = id;
   var queue = [[x, y, d]];
   while(queue.length !== 0){
     [x, y, d] = queue.splice(O.rand(queue.length), 1)[0];
-    if(d.id === id) continue;
+    if(d[0]) continue;
     d.id = id;
     var dirs = [];
+    var q = [];
     for(var dir = 0; dir < 4; dir++){
       var x1 = x + (dir === 1 ? -1 : dir === 3 ? 1 : 0);
       var y1 = y + (dir === 0 ? -1 : dir === 2 ? 1 : 0);
@@ -38,17 +49,20 @@ game.generate = () => {
       if(d1 === null) continue;
       var ddir1 = 1 << dir;
       var ddir2 = 1 << (dir + 2 & 3);
-      if(d1.id === id){
+      if(d1.id === id && (d1[0] || d1.id1 === id)){
         if((d[0] | ddir1) === 15 || (d1[0] | ddir2) === 15) continue;
         dirs.push([d1, dir]);
       }else{
-        queue.push([x1, y1, d1]);
+        q.push([x1, y1, d1]);
       }
     }
+    if(d.id1 === id || dirs.length !== 0) queue.push(...q);
     if(dirs.length !== 0){
       var [d1, dir] = dirs[O.rand(dirs.length)];
       d[0] |= 1 << dir;
       d1[0] |= 1 << (dir + 2 & 3);
+    }else{
+      queue.push([x, y, d]);
     }
   }
   game.iterate((x, y, d) => {
