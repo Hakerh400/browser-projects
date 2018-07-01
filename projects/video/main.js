7d6ac07b4f72bd852a8bd1ae643bf82eeff7832c
Hakerh400 <hakerh403@gmail.com>
2018-07-01 16:30:05 +0200

    commit

--- projects/video/main.js
+++ projects/video/main.js
@@ -6,43 +6,18 @@ var format = 'mp4';
 var videoName = getVideoName();
 var videoFile = `/projects/${O.project}/${videoName}.${format}`;
 
+var fs = O.urlParam('fs') === '1';
+
 var video = null;
 var source = null;
 var loaded = false;
 var playing = false;
 
-addEventListener('keydown', evt => {
-  if(!loaded) return;
-
-  switch(evt.code){
-    case 'Space':
-      if(playing) video.pause();
-      else video.play();
-      playing = !playing;
-      break;
-
-    case 'ArrowLeft':
-      video.currentTime -= TIME_INCREMENT;
-      break;
-
-    case 'ArrowRight':
-      video.currentTime += TIME_INCREMENT;
-      break;
-
-    case 'F5':
-      evt.preventDefault();
-      if(playing) video.pause();
-      loaded = false;
-      playing = false;
-      source.src = O.urlTime(videoFile);
-      video.load();
-      break;
-  }
-});
-
 window.setTimeout(main);
 
 function main(){
+  aels();
+
   O.body.style.margin = '0px';
   O.body.style.backgroundColor = 'black';
   O.body.style.overflow = 'hidden';
@@ -53,10 +28,49 @@ function main(){
   video.style.left = '50%';
   video.style.transform = 'translate(-50%, -50%)';
   video.loop = false;
-  video.addEventListener('canplay', () => { loaded = true; });
+
+  if(fs){
+    video.style.width = '100%';
+    video.style.height = '100%';
+  }
+
+  O.ael(video, 'canplay', () => {
+    loaded = true;
+  });
   
   source = O.ce(video, 'source');
   source.src = O.urlTime(videoFile);
+}
+
+function aels(){
+  O.ael('keydown', evt => {
+    if(!loaded) return;
+
+    switch(evt.code){
+      case 'Space':
+        if(playing) video.pause();
+        else video.play();
+        playing = !playing;
+        break;
+
+      case 'ArrowLeft':
+        video.currentTime -= TIME_INCREMENT;
+        break;
+
+      case 'ArrowRight':
+        video.currentTime += TIME_INCREMENT;
+        break;
+
+      case 'F5':
+        evt.preventDefault();
+        if(playing) video.pause();
+        loaded = false;
+        playing = false;
+        source.src = O.urlTime(videoFile);
+        video.load();
+        break;
+    }
+  });
 }
 
 function getVideoName(){
