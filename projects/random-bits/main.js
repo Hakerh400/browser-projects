4af4e518f0f422f428711f2dc583cd8fe0550a43
Hakerh400 <hakerh403@gmail.com>
2018-08-13 14:01:15 +0200

    commit

--- projects/random-bits/main.js
+++ projects/random-bits/main.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const MAX_LENGTH = 1e3;
+const MAX_LENGTH = 150;
 
 const FONT_SIZE = 32;
 const OFFSET = 8;
 
 const cols = {
   bg: '#ffffff',
-  input: '#000000',
-  output: '#a9a9a9',
+  text: '#000000',
 };
 
 const {w, h, g} = O.ceCanvas(1);
@@ -28,77 +27,25 @@ function main(){
 }
 
 function aels(){
-  var modes = O.enum([
-    'INPUT',
-    'OUTPUT',
-  ]);
+  var str = O.ca(MAX_LENGTH, () => {
+    return O.rand(2);
+  }).join('');
 
-  var mode = modes.INPUT;
-  var str = '';
+  updateStr();
 
   O.ael('keydown', evt => {
-    var code = evt.code;
-
-    switch(code){
-      case 'Backspace':
-        if(mode === modes.OUTPUT) str = '';
-        else str = str.substring(0, str.length - 1);
-        mode = modes.INPUT;
-        updateStr();
-        break;
-
-      case 'Delete':
+    switch(evt.code){
+      case 'ArrowRight':
         str = str.substring(1);
+        str += O.rand(2);
         updateStr();
-        break;
-
-      case 'Enter':
-        if(mode === modes.INPUT){
-          mode = modes.OUTPUT;
-          str = randBits();
-          updateStr();
-        }
-        break;
-
-      default:
-        var digit = code.match(/^(?:Digit|Numpad)(\d)$/);
-
-        if(digit !== null){
-          if(mode == modes.OUTPUT){
-            mode = modes.INPUT;
-            str = '';
-          }
-
-          digit = digit[1] | 0;
-          str += digit;
-
-          updateStr();
-          break;
-        }
-
         break;
     }
   });
 
   function updateStr(){
     clearCanvas();
-
-    var col = mode === modes.INPUT ? cols.input : cols.output;
-    drawStr(str, col);
-  }
-
-  function randBits(){
-    if(str.length === 0) return '';
-
-    var len = BigInt(str);
-    if(len === 0n) return '';
-
-    if(len > MAX_LENGTH) len = MAX_LENGTH;
-    len = Number(len);
-
-    return O.ca(len, () => {
-      return O.rand(2);
-    }).join('');
+    drawStr(str, cols.text);
   }
 }
 
