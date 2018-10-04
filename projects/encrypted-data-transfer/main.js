f293c7c7a46221b0ff6a93354bc351edcf0918f9
Hakerh400 <hakerh403@gmail.com>
2018-10-04 12:36:50 +0200

    commit

--- projects/encrypted-data-transfer/main.js
+++ projects/encrypted-data-transfer/main.js
@@ -3,13 +3,13 @@
 const dom = require('./dom');
 
 const REMOTE = 1;
-const VERSION = 2;
+const VERSION = 3;
 
 const UPDATE_INTERVAL = 1e3;
 const RETRY_NUM = 5;
 
 const URL = REMOTE ? 'https://e8kkzbh-encrypted-data-transfer.193b.starter-ca-central-1.openshiftapps.com/'
-                   : 'http://localhost:5000/';
+                   : 'http://localhost:8080/';
 
 var pass = null;
 
@@ -127,10 +127,37 @@ function displayMsgs(){
     
     O.ceText(dom.div(msgElem, 'msg-date'), new Date(date).toGMTString());
     O.ceText(dom.div(msgElem, 'msg-name'), `${decrypt(name)}:`);
-    O.ceText(dom.div(msgElem, 'msg-body'), decrypt(msg));
+
+    var msgBody = dom.div(msgElem, 'msg-body');
+    formatMsg(msgBody, decrypt(msg));
 
     msgsElem.scrollTo(0, msgsElem.scrollHeight);
   }
+}
+
+function formatMsg(elem, str){
+  str = str.replace(/\s+/g, ' ').trim();
+
+  var reg = /\[([^\]]+)\]\((https?\:\/\/[^\)]+)\)/;
+
+  while(1){
+    var match = str.match(reg);
+    if(match === null) break;
+
+    var {index} = match;
+    if(index !== 0)
+      O.ceText(elem, str.substring(0, index));
+
+    var link = O.ce(elem, 'a');
+    link.href = match[2];
+    link.target = '_blank';
+    O.ceText(link, match[1]);
+
+    str = str.substring(index + match[0].length);
+  }
+
+  if(str.length !== 0)
+    O.ceText(elem, str);
 }
 
 function encrypt(str){
