c14971d92a56f685a88c95eee28005de89ea5dc1
Thomas <hakerh403@gmail.com>
2018-11-21 17:46:10 +0100

    commit

--- projects/text-editor/main.js
+++ projects/text-editor/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const TAB_SIZE = 2;
+const PAGE_LINES = 70;
+const MAX_HISTORY_SIZE = 100;
+
 const TAB = ' '.repeat(TAB_SIZE);
+
+const SYMBOL_STATE = Symbol();
 
 window.setTimeout(main);
 
@@ -20,12 +25,40 @@ function onKeyDown(evt){
   evt.preventDefault();
 
   var ta = evt.target;
+
+  if(!(SYMBOL_STATE in ta)){
+    var state = O.obj();
+    ta[SYMBOL_STATE] = state;
+
+    state.pos = null;
+    state.clipboard = null;
+
+    state.history = [{type: 1, val: '', start: 0, end: 0}];
+    state.historyIndex = 0;
+  }
+
+  var state = ta[SYMBOL_STATE];
   var {key} = evt;
 
   var alt = evt.altKey;
   var ctrl = evt.ctrlKey;
   var shift = evt.shiftKey;
   var fKey = /^F\d+$/.test(key) ? key.slice(1) | 0 : 0;
+
+  var val = ta.value;
+  var len = val.length;
+  var pos = getPos();
+
+  var {history} = state;
+
+  var startPrev = getStart();
+  var endPrev = getEnd();
+  var size = endPrev - startPrev;
+
+  var update = shift;
+  var removePos = 1;
+  var saveHistory = 1;
+  var actionType = key.length === 1 && !(alt || ctrl || fKey) ? 0 : 1;
 
   if(fKey){
     switch(fKey){
@@ -37,7 +70,7 @@ function onKeyDown(evt){
     return;
   }
 
-  if(key.length === 1 && !(alt || ctrl || fKey)){
+  if(actionType === 0){
     var s = key;
 
     if(s === '(') s += ')';
@@ -46,149 +79,295 @@ function onKeyDown(evt){
     else if(/[)\]}'"`]/.test(s) && getCharRel(0) === s) s = '';
     else if(/['"`]/.test(s)) s += s;
 
+    if(size !== 0) remove(size);
+
     insert(s, 0);
     moveRel(1);
-
-    return;
-  }
-
-  var val = ta.value;
-  var len = val.length;
-  var pos = getPos();
-
-  var start = getStart();
-  var end = getEnd();
-  var size = end - start;
-  var update = shift;
-
-  switch(key){
-    case 'a':
-      if(!ctrl) break;
-      setStart(0);
-      setEnd(len);
-      break;
-
-    case 'M':
-      if(!ctrl) break;
-
-      var s = getCharRel(-1) + getCharRel(0);
-      if(/\(\)|\[\]|{}|['"`]{2}/.test(s)) break;
-
-      var d = 0;
-      if(/[\)\]}][^\(\)\[\]{}]/.test(s)) d = -1;
-      else if(/[^\(\)\[\]{}][\(\[{]/.test(s)) d = 1;
-      moveRel(d);
-
-      var depth = 0;
-      var start = find((c, dpos) => {
-        if(dpos === 0) return -1;
-        if(/[\(\[{]/.test(c)) depth--;
-        else if(/[\)\]}]/.test(c)) depth++;
-        if(depth === -1) return 0;
-        return -1;
-      });
-      if(depth !== -1){ moveRel(-d); break; }
-
-      depth = 0;
-      var end = find(c => {
-        if(/[\(\[{]/.test(c)) depth++;
-        else if(/[\)\]}]/.test(c)) depth--;
-        if(depth === -1) return 0;
-        return 1;
-      });
-      if(depth !== -1){ moveRel(-d); break; }
-
-      setStart(start + 1);
-      setEnd(end);
-
-      update = 0;
-      break;
-
-    case 'ArrowLeft':
-      if(!ctrl){
-        moveRel(-1, shift);
+    
+    update = 0;
+  }else{
+    switch(key){
+      case 'a':
+        if(!ctrl) break;
+        setStart(0);
+        setEnd(len);
         break;
-      }
-      moveAbs(getIdentStart());
-      break;
 
-    case 'ArrowRight':
-      if(!ctrl){
-        moveRel(1, shift);
-        break;
-      }
-      moveAbs(getIdentEnd());
-      break;
+      case 'M':
+        if(!ctrl) break;
 
-    case 'ArrowUp':
-      var start = getLineStart(1);
-      if(start === 0) break;
-      moveAbs(start - 1);
-      moveAbs(getLineStart(1));
-      moveAbs(find((c, dpos) => {
-        if(c === '\n' || dpos === pos - start) return 0;
-        return 1;
-      }));
-      break;
-
-    case 'ArrowDown':
-      var start = getLineStart(1);
-      var end = getLineEnd();
-      if(end === len) break;
-      moveAbs(end + 1);
-      moveAbs(find((c, dpos) => {
-        if(c === '\n' || dpos === pos - start) return 0;
-        return 1;
-      }));
-      break;
-
-    case 'Home':
-      moveAbs(getLineStart());
-      break;
-
-    case 'End':
-      moveAbs(getLineEnd());
-      break;
-
-    case 'Tab':
-      insert(TAB);
-      break;
-
-    case 'Backspace':
-      if(size !== 0){ remove(size); break; }
-      remove(-1);
-      break;
-
-    case 'Delete':
-      if(size !== 0){ remove(size); break; }
-      remove(1);
-      break;
-
-    case 'Enter':
-      if(pos !== 0 && pos !== len){
         var s = getCharRel(-1) + getCharRel(0);
-        if(/\(\)|\[\]|{}|['"`]{2}/.test(s)){
-          insert(`\n${TAB}\n`, 0);
-          moveRel(1);
-          moveAbs(getLineEnd());
+        if(/\(\)|\[\]|{}|['"`]{2}/.test(s)) break;
+
+        var d = 0;
+        if(/[\)\]}][^\(\)\[\]{}]/.test(s)) d = -1;
+        else if(/[^\(\)\[\]{}][\(\[{]/.test(s)) d = 1;
+        moveRel(d);
+
+        var depth = 0;
+        var start = find((c, dpos) => {
+          if(dpos === 0) return -1;
+          if(/[\(\[{]/.test(c)) depth--;
+          else if(/[\)\]}]/.test(c)) depth++;
+          if(depth === -1) return 0;
+          return -1;
+        });
+        if(depth !== -1){ moveRel(-d); break; }
+
+        depth = 0;
+        var end = find(c => {
+          if(/[\(\[{]/.test(c)) depth++;
+          else if(/[\)\]}]/.test(c)) depth--;
+          if(depth === -1) return 0;
+          return 1;
+        });
+        if(depth !== -1){ moveRel(-d); break; }
+
+        setStart(start + 1);
+        setEnd(end);
+
+        update = 0;
+        break;
+
+      case 'c':
+        if(!ctrl) break;
+        var start, end;
+        if(size === 0){
+          start = getLineStart(1);
+          end = getLineEnd();
+        }else{
+          start = startPrev;
+          end = endPrev;
+        }
+        state.clipboard = val.slice(start, end);
+        break;
+
+      case 'x':
+        if(!ctrl) break;
+        var start, end;
+        if(size === 0){
+          start = getLineStart(1);
+          end = getLineEnd() + 1;
+        }else{
+          start = startPrev;
+          end = endPrev;
+        }
+        state.clipboard = val.slice(start, end);
+        ta.value = val.slice(0, start) + val.slice(end);
+        moveAbs(start);
+        update = 0;
+        break;
+
+      case 'v':
+        if(!ctrl) break;
+        var s = state.clipboard;
+        if(s === null) break;
+        if(size !== 0) remove(size);
+        insert(s);
+        update = 0;
+        break;
+
+      case 'D':
+        if(!ctrl) break;
+        var start = getLineStart(1);
+        var end = getLineEnd();
+        var s = val.slice(start, end);
+        moveAbs(end);
+        insert(`\n${s}`, 0, 0);
+        moveAbs(pos + s.length + 1);
+        update = 0;
+        break;
+
+      case 'z':
+        if(!ctrl) break;
+        if(state.historyIndex === 0) break;
+        var {val, start, end} = history[--state.historyIndex];
+        ta.value = val;
+        setStart(start);
+        setEnd(end);
+        update = 0;
+        saveHistory = 0;
+        break;
+
+      case 'y':
+        if(!ctrl) break;
+        if(state.historyIndex === history.length - 1) break;
+        var {val, start, end} = history[++state.historyIndex];
+        ta.value = val;
+        setStart(start);
+        setEnd(end);
+        update = 0;
+        saveHistory = 0;
+        break;
+
+      case 'ArrowLeft':
+        if(ctrl) moveAbs(getIdentStart());
+        else moveRel(-1, shift);
+        break;
+
+      case 'ArrowRight':
+        if(ctrl) moveAbs(getIdentEnd());
+        else moveRel(1, shift);
+        break;
+
+      case 'ArrowUp':
+        var start = getLineStart(1);
+        var end = getLineEnd();
+        if(start === 0) break;
+
+        if(ctrl && shift){
+          update = 0;
+          moveAbs(start - 1);
+
+          var start1 = getLineStart(1);
+          var end1 = getLineEnd();
+          var s = val.slice(start, end);
+          var s1 = val.slice(start1, end1);
+
+          ta.value = `${val.slice(0, start1)}${s}\n${s1}${val.slice(end)}`;
+
+          var dpos = -(s1.length + 1);
+          setStart(startPrev + dpos);
+          setEnd(endPrev + dpos);
           break;
         }
-      }
-      insert('\n');
-      break;
+
+        if(state.pos === null) state.pos = pos - start;
+        moveAbs(start - 1);
+        moveAbs(getLineStart(1));
+        moveAbs(find((c, dpos) => {
+          if(c === '\n' || dpos === state.pos) return 0;
+          return 1;
+        }));
+        removePos = 0;
+        break;
+
+      case 'ArrowDown':
+        var start = getLineStart(1);
+        var end = getLineEnd();
+        if(end === len) break;
+
+        if(ctrl && shift){
+          update = 0;
+          moveAbs(end + 1);
+
+          var start1 = getLineStart(1);
+          var end1 = getLineEnd();
+          var s = val.slice(start, end);
+          var s1 = val.slice(start1, end1);
+
+          ta.value = `${val.slice(0, start)}${s1}\n${s}${val.slice(end1)}`;
+
+          var dpos = s1.length + 1;
+          setStart(startPrev + dpos);
+          setEnd(endPrev + dpos);
+          break;
+        }
+
+        if(state.pos === null) state.pos = pos - start;
+        moveAbs(end + 1);
+        moveAbs(find((c, dpos) => {
+          if(c === '\n' || dpos === state.pos) return 0;
+          return 1;
+        }));
+        removePos = 0;
+        break;
+
+      case 'Home':
+        moveAbs(getLineStart());
+        break;
+
+      case 'End':
+        moveAbs(getLineEnd());
+        break;
+
+      case 'PageUp':
+        var n = PAGE_LINES;
+        moveAbs(find(c => {
+          if(c === '\n') n--;
+          if(n === 0) return 0;
+          return -1;
+        }));
+        moveAbs(getLineStart(1));
+        break;
+
+      case 'PageDown':
+        var n = PAGE_LINES;
+        moveAbs(find(c => {
+          if(c === '\n') n--;
+          if(n === 0) return 0;
+          return 1;
+        }));
+        moveAbs(getLineEnd());
+        break;
+
+      case 'Tab':
+        insert(TAB);
+        break;
+
+      case 'Backspace':
+        update = 0;
+        if(size !== 0){ remove(size); break; }
+        remove(-1);
+        break;
+
+      case 'Delete':
+        update = 0;
+        if(size !== 0){ remove(size); break; }
+        remove(1);
+        break;
+
+      case 'Enter':
+        if(pos !== 0 && pos !== len){
+          var s = getCharRel(-1) + getCharRel(0);
+          if(/\(\)|\[\]|{}|['"`]{2}/.test(s)){
+            insert(`\n${TAB}\n`, 0);
+            moveRel(1);
+            moveAbs(getLineEnd());
+            break;
+          }
+        }
+        insert('\n');
+        break;
+    }
   }
 
   if(update){
-    setStart(Math.min(getStart(), start));
-    setEnd(Math.max(getEnd(), end));
+    setStart(Math.min(getStart(), startPrev));
+    setEnd(Math.max(getEnd(), endPrev));
   }
 
-  function insert(str, move=1){
+  if(removePos){
+    state.pos = null;
+  }
+
+  if(saveHistory){
+    var {type, val, start, end} = history[state.historyIndex];
+    var obj = {type: actionType, val: ta.value, start: getStart(), end: getEnd()};
+
+    if(val !== obj.val){
+      if(state.historyIndex !== history.length - 1){
+        history.splice(state.historyIndex + 1);
+        state.historyIndex = history.length - 1;
+      }
+
+      if(history.length === MAX_HISTORY_SIZE || type === 0 && actionType === 0){
+        history[state.historyIndex] = obj;
+      }else{
+        history.push(obj);
+        state.historyIndex++;
+      }
+    }
+  }
+
+  function insert(str, move=1, addTabs=1){
     var pos = getPos();
     var tab = getTab();
     var s = ta.value;
 
-    str = str.replace(/\n/g, `\n${tab}`);
+    if(addTabs)
+      str = str.replace(/\n/g, `\n${tab}`);
+
     s = s.slice(0, pos) + str + s.slice(pos);
     ta.value = s;
 
