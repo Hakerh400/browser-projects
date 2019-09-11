5e2934d59b8ac9b670d2139149b413a6ac5c6d8f
Thomas <hakerh403@gmail.com>
2019-09-11 08:15:55 +0200

    commit

--- projects/realms/realm.js
+++ projects/realms/realm.js
@@ -3,7 +3,7 @@
 class Realm{
   get name(){ O.virtual('name'); }
   get ctors(){ O.virtual('ctors'); }
-  createGen(start, pset){ O.virtual('createGenerator'); }
+  createGen(start, pset){ O.virtual('createGen'); }
 }
 
 module.exports = Realm;
