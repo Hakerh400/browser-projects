5f83de9c079dd50d7bcd496b98bbfc4eb9e26cfb
Thomas <hakerh403@gmail.com>
2020-01-28 20:45:46 +0100

    commit

--- projects/circular-tree/tree.js
+++ projects/circular-tree/tree.js
@@ -4,24 +4,21 @@ const Node = require('./node');
 
 class Tree{
   constructor(){
-    const left = a => {
-      const b = new Uint8ClampedArray(a);
-      b[0] = (b[0] + 255) / 2;
-      b[1] = (b[0] + b[2]) / 2;
-      b[2] = (b[0] + b[1]) / 3;
-      return new Node(b, () => left(b), () => right(b));
+    const left = (k, n) => {
+      k -= 2 ** (-n);
+      n++;
+
+      return new Node(O.hsv(k), () => left(k, n), () => right(k, n));
     };
 
-    const right = a => {
-      const b = new Uint8ClampedArray(a);
-      b[0] += 10;
-      b[1] += 50;
-      b[2] += 100;
-      return new Node(b, () => left(b), () => right(b));
+    const right = (k, n) => {
+      k += 2 ** (-n);
+      n++;
+
+      return new Node(O.hsv(k), () => left(k, n), () => right(k, n));
     };
 
-    const col = new Uint8ClampedArray([0, 0, 0]);
-    this.root = new Node(col, () => left(col), () => right(col));
+    this.root = new Node(O.hsv(.5), () => left(.5, 2), () => right(.5, 2));
   }
 
   get(id, depth){
